d4a12e0ed74408a61c5f25a9f6961085e647dc0b
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-05 16:43:54 +1000

    Test invocation using a single argument

--- test/all.js
+++ test/all.js
@@ -11,3 +11,14 @@ test('can trigger an indirect call with no arguments', function(t) {
 
   fn();
 });
+
+test('can trigger an indirect with numeric arguments', function(t) {
+  var fn;
+
+  t.plan(1);
+  fn = indirect(function(val) {
+    t.equal(val, 5);
+  });
+
+  fn(5);
+});
